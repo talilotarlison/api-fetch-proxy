b067bec070ea6bf7fc00c006eb80296321b9e562
Talilo Tarlison <101112484+talilotarlison@users.noreply.github.com>
2023-06-02 10:10:02 -0300

    Create api_fetch_github.js

---
+++ api_fetch_github.js
@@ -0,0 +1,36 @@
+/*function main(){
+  const user = "talilotarlison"
+  const url = "https://api.github.com/users/"
+  fetch(url+user) 
+    .then(async (response)=>{
+return await response.json()
+    //console.log(res)
+  }).then((user)=>{console.log(user.bio)})
+}
+
+
+async function main(){
+    const user = "talilotarlison"
+  const url = "https://api.github.com/users/"
+  const promise = await fetch(url+user)
+  const usuario = await promise.json()
+  console.log(usuario)
+}
+*/
+
+async function main(){
+    const user1 = "talilotarlison"
+    const user2 = "rprrafa"
+  const url = "https://api.github.com/users/"
+  const promise1 =  fetch(url+user1)
+  const promise2 =  fetch(url+user2)
+  const promises = await Promise.all([promise1 ,promise2]);
+ const res1 =  await promises[1].json()
+ const res2 =   await promises[0].json()
+  
+  console.log(res1.id)
+  console.log(res2.id) 
+}
+
+main()
+    
